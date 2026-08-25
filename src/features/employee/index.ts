f3d7aaf363cc65167b046f724e37d0cd0d181cb8
ᔕYᖇIᗩᑎ <35205275+AbuNizar963@users.noreply.github.com>
2026-08-25 10:10:59 +0300

    refactor: define employee public feature API

---
+++ src/features/employee/index.ts
@@ -0,0 +1,6 @@
+export { default as EmployeeHome } from "@/features/employee/pages/EmployeeHome";
+export { default as EmployeeHistory } from "@/features/employee/pages/EmployeeHistory";
+export { default as EmployeeScanAutoFlow } from "@/features/employee/pages/EmployeeScanAutoFlow";
+export { default as EmployeeProfile } from "@/features/employee/pages/EmployeeProfile";
+export { default as EmployeeCenter } from "@/features/employee/pages/EmployeeCenter";
+export { default as EmployeePremium } from "@/features/employee/pages/EmployeePremium";
