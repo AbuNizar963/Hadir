1da57aaf01fcd676b6f08d0a0c2e3a9aa9c9f61c
ᔕYᖇIᗩᑎ <35205275+AbuNizar963@users.noreply.github.com>
2026-08-25 10:11:04 +0300

    refactor: define manager public feature API

---
+++ src/features/manager/index.ts
@@ -0,0 +1,7 @@
+export { default as ManagerDashboard } from "@/features/manager/pages/ManagerDashboard";
+export { default as ManagerEmployees } from "@/features/manager/pages/ManagerEmployees";
+export { default as ManagerRequests } from "@/features/manager/pages/ManagerRequests";
+export { default as ManagerAudit } from "@/features/manager/pages/ManagerAudit";
+export { default as ManagerSettings } from "@/features/manager/pages/ManagerSettings";
+export { default as ManagerReports } from "@/features/manager/pages/ManagerReports";
+export { default as ManagerLogin } from "@/features/manager/pages/ManagerLogin";
