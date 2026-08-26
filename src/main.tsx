38e9f5a606c28190fbc20d5709f0f49102d06b17
ᔕYᖇIᗩᑎ <35205275+AbuNizar963@users.noreply.github.com>
2026-08-27 02:13:46 +0300

    feat(system): protect app shell from render failures

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import AppErrorBoundary from "./components/system/AppErrorBoundary";
 import "./index.css";
 import "./live-ui.css";
 import "./label-visibility.css";
@@ -33,6 +34,8 @@ window.addEventListener("unhandledrejection", (event) => {
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <AppErrorBoundary>
+      <App />
+    </AppErrorBoundary>
   </StrictMode>
 );
