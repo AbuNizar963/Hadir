1ddd0cd4faa5979ceee79346622fdc634d9f9191
ᔕYᖇIᗩᑎ <35205275+AbuNizar963@users.noreply.github.com>
2026-09-08 04:21:36 +0300

    fix(flutter): resolve Excel and Flutter Border import conflict

--- mobile/flutter/lib/pages/admin_reports_page.dart
+++ mobile/flutter/lib/pages/admin_reports_page.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'dart:typed_data';
 
-import 'package:excel/excel.dart';
+import 'package:excel/excel.dart' hide Border;
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:share_plus/share_plus.dart';
