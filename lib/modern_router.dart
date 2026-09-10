57d27bebe1db9b5c565a6aeda8a74fd7b5f37218
ᔕYᖇIᗩᑎ <35205275+AbuNizar963@users.noreply.github.com>
2026-09-10 19:42:16 +0300

    fix: remove unused legacy settings import

--- lib/modern_router.dart
+++ lib/modern_router.dart
@@ -14,7 +14,6 @@ import 'pages/admin_management_page.dart';
 import 'pages/admin_operations_page.dart';
 import 'pages/admin_reports_page.dart';
 import 'pages/admin_report_archive_page.dart';
-import 'pages/admin_settings_page.dart';
 import 'pages/attendance_page.dart';
 import 'pages/attendance_insights_page.dart';
 import 'pages/employee_center_page.dart';
