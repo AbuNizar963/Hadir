4cfbf72e48ce1d90a287074b7b405f872743922a
ᔕYᖇIᗩᑎ <35205275+AbuNizar963@users.noreply.github.com>
2026-09-12 23:09:26 +0300

    feat(time): initialize HADIR timezone database

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/widgets.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:timezone/data/latest.dart' as tz;
 
 import 'app/hadir_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  tz.initializeTimeZones();
   await initializeDateFormatting('ar', null);
   runApp(const HadirApp());
 }
